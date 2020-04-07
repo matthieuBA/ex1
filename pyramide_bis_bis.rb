e1126a75063b134cc1dcc8e1a2b0af62c9bbc9ca
matthieuBA <matthieubacheley@gmail.com>
2020-04-07 20:00:11 +0200

    final commit

--- pyramide_bis_bis.rb
+++ pyramide_bis_bis.rb
@@ -2,11 +2,10 @@ puts "nombre"
 print "> "
 n = Integer(gets.chomp)+1
 n.times do |i|
-    print " "*(n-i)
-    print "#"*i
-    # if i>=1 then
-    #     print "#"*(i-1)
-    # end
+    print " "*(n-i)+"#"*i
+    if i>=1 then
+        print "#"*(i-1)
+    end
     print " "*(n-i)
     puts ""
 end
